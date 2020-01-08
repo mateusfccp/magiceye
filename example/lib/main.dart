bf9f27ffb3621d608f1fd809b7a7cfe130714a9f
Mateus Felipe <mateus.pinto@iatec.com>
2020-01-08 08:41:45 -0300

    Fix example when backpressing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,30 +31,35 @@ class LoucoButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text("Take picture"),
-            onPressed: () => MagicEye().push(context).then(
-                  (path) => path.fold(
-                    (_) => null,
-                    (path) => file.add(
-                      File(path),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Take Picture"),
+              onPressed: () => MagicEye().push(context).then(
+                    (path) => path?.fold(
+                      (_) => null,
+                      (path) => file.add(
+                        File(path),
+                      ),
                     ),
                   ),
-                ),
-          ),
-          SizedBox(height: 16),
-          StreamBuilder<File>(
-            stream: file.stream,
-            initialData: null,
-            builder: (context, snapshot) => snapshot.hasData
-                ? Image.file(snapshot.data)
-                : Container(color: Colors.grey),
-          ),
-        ],
+            ),
+            SizedBox(height: 16),
+            StreamBuilder<File>(
+              stream: file.stream,
+              initialData: null,
+              builder: (context, snapshot) => snapshot.hasData
+                  ? Image.file(snapshot.data)
+                  : AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: Container(color: Colors.grey[300]),
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/layers/default_camera_control_layer.dart
+++ lib/src/layers/default_camera_control_layer.dart
@@ -9,110 +9,123 @@ import '../contexts/control_layer_context.dart';
 import '../enums/device_direction.dart';
 import '../extra/circle_button.dart';
 
-Widget defaultCameraControlLayer(
-  BuildContext context,
-  ControlLayerContext layerContext,
-) {
-  BehaviorSubject<Option<String>> pathStream = BehaviorSubject.seeded(None());
+Widget Function(BuildContext, ControlLayerContext) defaultCameraControlLayer() {
+  BehaviorSubject<Option<Future<Either<MagicEyeException, String>>>>
+      pathStream = BehaviorSubject.seeded(None());
+  return (
+    BuildContext context,
+    ControlLayerContext layerContext,
+  ) {
+    Widget _bottomPictureButtons(
+            BuildContext context, ControlLayerContext layerContext) =>
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              CircleButton(
+                icon: Icons.arrow_back_ios,
+                onPressed: () => Navigator.of(context).pop,
+                orientationStream: layerContext.direction,
+              ),
+              StreamBuilder<DeviceDirection>(
+                initialData: layerContext.direction.value,
+                stream: layerContext.direction,
+                builder: (context, snapshot) {
+                  final bool enabled =
+                      layerContext.allowedDirections.contains(snapshot.data);
 
-  Widget _bottomPictureButtons(
-          BuildContext context, ControlLayerContext layerContext) =>
-      Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            CircleButton(
-              icon: Icons.arrow_back_ios,
-              onPressed: () => Navigator.of(context).pop,
-              orientationStream: layerContext.direction,
-            ),
-            StreamBuilder<DeviceDirection>(
-              initialData: DeviceDirection.portrait,
-              stream: layerContext.direction,
-              builder: (context, snapshot) {
-                final bool enabled =
-                    layerContext.allowedDirections.contains(snapshot.data);
-
-                return AnimatedCrossFade(
-                  duration: Duration(milliseconds: 500),
-                  firstCurve: Curves.easeOutQuint,
-                  firstChild: CircleButton(
-                    icon: Icons.camera_alt,
-                    onPressed: () => layerContext.takePicture().then(
-                          (pathOr) => pathOr.fold(
-                            (error) => Navigator.of(context)
-                                .pop<Either<MagicEyeException, String>>(
-                                    Left(error)),
-                            (path) => pathStream.add(Some(path)),
-                          ),
+                  return AnimatedCrossFade(
+                    duration: Duration(milliseconds: 500),
+                    firstCurve: Curves.easeOutQuint,
+                    firstChild: CircleButton(
+                      icon: Icons.camera_alt,
+                      onPressed: () => pathStream.add(
+                        Some(
+                          layerContext.takePicture(),
                         ),
-                    orientationStream: layerContext.direction,
-                  ),
-                  secondCurve: Curves.easeOutQuint,
-                  secondChild: SizedBox(width: 50, height: 50),
-                  crossFadeState: enabled
-                      ? CrossFadeState.showFirst
-                      : CrossFadeState.showSecond,
-                );
-              },
-            ),
-            layerContext.allowedCameras.length > 1
-                ? CircleButton(
-                    icon: Icons.cached,
-                    onPressed: layerContext.switchCamera,
-                    orientationStream: layerContext.direction,
-                  )
-                : SizedBox(width: 50, height: 50),
-          ],
-        ),
-      );
+                      ),
+                      orientationStream: layerContext.direction,
+                    ),
+                    secondCurve: Curves.easeOutQuint,
+                    secondChild: SizedBox(width: 50, height: 50),
+                    crossFadeState: enabled
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+                  );
+                },
+              ),
+              layerContext.allowedCameras.length > 1
+                  ? CircleButton(
+                      icon: Icons.cached,
+                      onPressed: layerContext.switchCamera,
+                      orientationStream: layerContext.direction,
+                    )
+                  : SizedBox(width: 50, height: 50),
+            ],
+          ),
+        );
 
-  Widget _bottomConfirmationButtons(BuildContext context, String path) =>
-      Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            CircleButton(
-              icon: Icons.close,
-              onPressed: () {
-                final Directory directory = new Directory(path);
-                directory
-                    .delete(recursive: true)
-                    .then((_) => pathStream.add(None()));
-              },
-            ),
-            SizedBox(width: 50, height: 50),
-            CircleButton(
-              icon: Icons.check,
-              onPressed: () {
-                pathStream.close();
-                Navigator.of(context)
-                    .pop<Either<MagicEyeException, String>>(Right(path));
-              },
-            )
-          ],
-        ),
-      );
+    Widget _bottomConfirmationButtons(BuildContext context, String path) =>
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              CircleButton(
+                icon: Icons.close,
+                onPressed: () {
+                  final Directory directory = new Directory(path);
+                  directory
+                      .delete(recursive: true)
+                      .then((_) => pathStream.add(None()));
+                },
+              ),
+              SizedBox(width: 50, height: 50),
+              CircleButton(
+                icon: Icons.check,
+                onPressed: () {
+                  pathStream.close();
+                  Navigator.of(context)
+                      .pop<Either<MagicEyeException, String>>(Right(path));
+                },
+              )
+            ],
+          ),
+        );
 
-  return Material(
-    type: MaterialType.transparency,
-    child: Container(
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
         width: double.infinity,
-        child: StreamBuilder<Option<String>>(
-          initialData: None(),
+        child: StreamBuilder<Option<Future<Either<MagicEyeException, String>>>>(
+          initialData: pathStream.value,
           stream: pathStream,
           builder: (context, snapshot) => snapshot.data.fold(
             () => _bottomPictureButtons(context, layerContext),
-            (path) => Stack(
-              alignment: Alignment.bottomCenter,
-              children: <Widget>[
-                Image.file(File(path)),
-                _bottomConfirmationButtons(context, path),
-              ],
+            (pathOr) => FutureBuilder<Either<MagicEyeException, String>>(
+              future: pathOr,
+              builder: (context, snapshot) => Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  Container(color: Colors.black),
+                  ...(snapshot.hasData && snapshot.data.isRight()
+                      ? [
+                          Image.file(
+                            File(snapshot.data.getOrElse(() => '')),
+                          ),
+                          _bottomConfirmationButtons(
+                            context,
+                            snapshot.data.getOrElse(() => ''),
+                          ),
+                        ]
+                      : [Center(child: CircularProgressIndicator())]),
+                ],
+              ),
             ),
           ),
-        )),
-  );
+        ),
+      ),
+    );
+  };
 }
