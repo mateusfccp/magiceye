4f58a0ee268d3ce574bb1d9807dd718c93646fe6
Mateus Felipe <mateus.pinto@iatec.com>
2020-01-23 15:16:16 -0300

    Export default layers

--- lib/magiceye.dart
+++ lib/magiceye.dart
@@ -5,4 +5,6 @@ export './src/contexts/preview_layer_context.dart';
 export './src/enums/device_camera.dart';
 export './src/enums/device_direction.dart';
 export './src/widget/magiceye_widget.dart';
+export './src/layers/default_camera_control_layer.dart';
+export './src/layers/default_camera_preview_layer.dart';
 export './src/layers/preview_layer.dart';
